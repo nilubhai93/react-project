b05352139f7921b398531c76df8ecce26f13f376
nilubhai93 <niladrimaji9382@gmail.com>
2025-08-24 02:24:48 +0530

    ecom

--- eCommerce Project/src/App.jsx
+++ eCommerce Project/src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./Components/Home/Home";
 const App =() =>{
   return(
     <>
-     
      <Home/>
     </>
   )
--- eCommerce Project/src/Components/Navbar/Navbar.jsx
+++ eCommerce Project/src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ import { HiMiniShoppingBag } from "react-icons/hi2";
 
 const Navbar = () => {
   return (
-    <header className='bg-red-300'>
+    <header className='bg-red-100 fixed top-0 right-0 left-0'>
       <nav className=' max-w-[1300px] px-12 mx-auto h-[14vh] flex items-center justify-between'>
         {/* logo */}
         <a href="#" className=' flex w-15 h-15 bg-zinc-300 rounded-full p-2'>
@@ -20,29 +20,31 @@ const Navbar = () => {
         <div className='flex items-center justify-center gap-x-5'>
           
           {/* search bar */}
-          <div className='p-1 rounded-full flex justify-center items-center border-2 border-solid border-blue-500'>
+          <div className='p-1 rounded-full flex justify-center items-center border-2 border-solid' >
             <input
               type="text"
               name="search"
               id="search"
               placeholder='Search...'
               autoComplete='off' 
-              className='h-[5vh] pl-4'
+              className='h-[5vh] pl-4 focus:outline-none'
             />
 
-            <button>
+            <button className='flex justify-center items-center w-10 h-10 rounded-full  bg-blue-600 text-white text-xl'>
               <IoMdSearch />
             </button>
           </div>
 
           {/* icons */}
-          <button className='text-[1.7rem] text-zinc-800'>
+          <button className='text-[1.7rem] text-zinc-800 relative'>
             <GoHeartFill/>
+            <span className=' flex justify-center bg-red-600 text-white w-5 h-5 rounded-full  text-[14px] absolute bottom-4 left-4'>1</span>
           </button>
 
           {/* shopping bag icon*/}
-          <button className='text-[1.7rem] text-zinc-800'>
+          <button className='text-[1.7rem] text-zinc-800 relative'>
             <HiMiniShoppingBag />
+            <span className=' flex justify-center bg-red-600 text-white w-5 h-5 rounded-full  text-[14px] absolute bottom-4 left-4'>1</span>
           </button>
           
         </div>
