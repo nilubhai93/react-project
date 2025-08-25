062a62fd3c79b89249393a5e9c56310b0288cef8
nilubhai93 <niladrimaji9382@gmail.com>
2025-08-26 03:19:44 +0530

    ecom

--- eCommerce Project/src/Components/Products/Product.jsx
+++ eCommerce Project/src/Components/Products/Product.jsx
@@ -1,11 +1,44 @@
 import React, { useState } from 'react'
+import ProductList from "./ProductList.js"
+import { GoHeartFill } from 'react-icons/go';
 
 const Product = () => {
 
     const categories = ["All", "Mens", "Womens", "Kids", "New Arrivals", "On Sale"]
-    const [activeTab, setActiveTab] = useState("All")
+    const [activeTab, setActiveTab] = useState("All");
+    const renderProducts = ProductList.map(product => {
+        return (
+            // card
+            <div className='bg-zinc-100 p-5 border-zinc-300 rounded-lg'>
+                <div className='flex justify-between items-center'>
+                    <button className='text-3xl text-zinc-300'>
+                        <GoHeartFill />
+                    </button>
+                    <span className='bg-red-600 px-3 py-1 text-white'>Sale</span>
+                </div>
+
+
+                {/* product image */}
+                <div className=' w-full h-[30vh]'>
+                    <img src={product.image}/>
+                </div>
+
+
+                {/* product details */}
+                <div className='text-center mt-3'>
+                    <h3 className='text-[1.4rem] font-semibold sm:text-sm '>{product.name}</h3>
+                    <div className='mt-1 mb-5'>
+                        <span className='text-red-600 font-semibold text-lg'>  ₹{product.price}</span>
+                    </div>
+                    <button className='bg-blue-600 text-white text-lg py-1 w-full rounded-lg cursor-pointer active:bg-blue-800' > Add to Cart</button>
+                </div>
+
+            </div>
+        )
+    })
     return (
         <section className='max-w-[1300px] mx-auto px-12 py-10'>
+
             {/* Tab */}
             <div className='flex gap-3 justify-center items-center mt-8'>
                 {
@@ -14,12 +47,17 @@ const Product = () => {
                             <button key={category} className={` px-8 py-2 rounded-full text-lg whitespace-nowrap cursor-pointer
                             ${activeTab === category ? "bg-blue-600 text-white" : "bg-zinc-100 text-zinc-800"}`}
                                 onClick={() => setActiveTab(category)}>
-
                                 {category}
                             </button>
                         )
                     })
                 }
+            </div>
+
+
+            {/* product listing */}
+            <div className='grid grid-cols-4 gap-9 mt-12'>
+                {renderProducts}
             </div>
         </section>
     )
---
+++ eCommerce Project/src/Components/Products/ProductList.js
@@ -0,0 +1,138 @@
+import Jeans from '../../assets/Jeans.png'
+import Hoodie from '../../assets/hoodie.png'
+import LeatherJacket from '../../assets/leather-jacket.png'
+import Skirt from '../../assets/skirt.png'
+import TShirt from '../../assets/tshirt.png'
+import Sleepsuit from '../../assets/sleepsuit.png'
+import Shirt from '../../assets/shirt.png'
+import Dress from '../../assets/dress.png'
+import Skater from '../../assets/skater.png'
+import Sweater from '../../assets/sweater.png'
+import BabyShirt from '../../assets/baby-shirt.png'
+import ShirtDress from '../../assets/shirt-dress.png'
+
+
+const products = [
+    {
+        id: 1,
+        name: "Sports TShirt",
+        image: TShirt,
+        price: 25.99,
+        oldPrice: 35.99,
+        onSale: true,
+        newArrival: false,
+        category: "Mens",
+    },
+    {
+        id: 2,
+        name: "Casual Jeans",
+        image: Jeans,
+        price: 45.00,
+        oldPrice: 60.00,
+        onSale: true,
+        newArrival: false,
+        category: "Mens",
+    },
+    {
+        id: 3,
+        name: "Winter Hoodie",
+        image: Hoodie,
+        price: 30.00,
+        oldPrice: 50.00,
+        onSale: true,
+        newArrival: true,
+        category: "Mens",
+    },
+    {
+        id: 4,
+        name: "Leather Jacket",
+        image: LeatherJacket,
+        price: 120.00,
+        oldPrice: 150.00,
+        onSale: true,
+        newArrival: false,
+        category: "Mens",
+    },
+    {
+        id: 5,
+        name: "Stylish Skirt",
+        image: Skirt,
+        price: 20.00,
+        oldPrice: 35.00,
+        onSale: true,
+        newArrival: true,
+        category: "Womens",
+    },
+    {
+        id: 6,
+        name: "Baby Sleepsuit",
+        image: Sleepsuit,
+        price: 15.00,
+        oldPrice: 25.00,
+        onSale: true,
+        newArrival: false,
+        category: "Kids",
+    },
+    {
+        id: 7,
+        name: "Formal Shirt",
+        image: Shirt,
+        price: 40.00,
+        oldPrice: 55.00,
+        onSale: true,
+        newArrival: false,
+        category: "Mens",
+    },
+    {
+        id: 8,
+        name: "Stripes Sweater",
+        image: Sweater,
+        price: 65.00,
+        oldPrice: 85.00,
+        onSale: true,
+        newArrival: false,
+        category: "Kids",
+    },
+    {
+        id: 9,
+        name: "Party Dress",
+        image: Dress,
+        price: 70.00,
+        oldPrice: 90.00,
+        onSale: true,
+        newArrival: true,
+        category: "Womens",
+    },
+    {
+        id: 10,
+        name: "Skater Outfit",
+        image: Skater,
+        price: 35.00,
+        oldPrice: 50.00,
+        onSale: true,
+        newArrival: false,
+        category: "Mens",
+    },
+    {
+        id: 11,
+        name: "Baby Shirt",
+        image: BabyShirt,
+        price: 12.00,
+        oldPrice: 20.00,
+        onSale: true,
+        newArrival: true,
+        category: "Kids",
+    },
+    {
+        id: 12,
+        name: "Full Sleeve Shirt",
+        image: ShirtDress,
+        price: 10.00,
+        oldPrice: 18.00,
+        onSale: true,
+        newArrival: false,
+        category: "Womens",
+    },
+];
+
+export default  products
